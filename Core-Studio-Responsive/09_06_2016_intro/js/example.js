b85b3c87d46da134f96a6958cc705833ab95a693
gaelhugo <gael.hugo@onemore-studio.com>
2016-09-06 15:29:15 +0200

    Building a grid

---
+++ Core-Studio-Responsive/09_06_2016_intro/js/example.js
@@ -0,0 +1,44 @@
+// VARIABLES
+var words  = "blabla";
+var number = 1;
+console.log(number);
+number = 2;
+console.log(number);
+number = number + 10;
+console.log(number);
+
+
+//functions
+function addition(a,b){
+    var c = a * b;
+    return c;
+}
+
+var result = addition(23,23457);
+console.log(result);
+console.log("-------------");
+
+
+//loop
+for(var i = 0;i<=100;i++){
+  //console.log(i);
+  var newResult = addition(i,i);
+  console.log(newResult);
+}
+
+
+//DRAWING
+var canvas = document.getElementById("gael");
+canvas.width = window.innerWidth;
+canvas.height= window.innerHeight;
+var ctx = canvas.getContext("2d");
+var side = 20;
+ctx.strokeStyle = 'black';
+
+for(var j = 0;j<70;j++){
+  for(var i = 0;i<70;i++){
+    ctx.rect(side*i,side*j,side,side);
+  }
+}
+
+ctx.stroke();
